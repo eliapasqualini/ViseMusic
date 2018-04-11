175847588a634edf08089d0ab5d3f3adb2870bfa
Mattia Navacchia <mattia.navacchia@studio.unibo.it>
2018-04-11 15:11:25 +0200

    Add PlayerController class

--- src/org/vise/controller/PlayerControllerImpl.java
+++ src/org/vise/controller/PlayerControllerImpl.java
@@ -1,10 +1,58 @@
 package org.vise.controller;
 
+import org.vise.model.FileSystemHandler;
+
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+
 /**
  * 
  * @author mattianavach
  *
  */
 public class PlayerControllerImpl {
+
+    private final AudioPlayer player;
+    private boolean isPlaying;
+
+    /**
+     * 
+     */
+    public PlayerControllerImpl() {
+        final Minim minim = new Minim(new FileSystemHandler());
+        this.player = minim.loadFile("res/songs/song1.mp3");
+        this.isPlaying = false;
+    }
+
+    /**
+     * Play song.
+     */
+    public void play() {
+        if (!this.isPlaying)  {
+            this.player.play();
+            this.setPlay(true);
+        } else {
+            this.player.pause();
+            this.setPlay(false);
+        }
+    }
+
+    /**
+     * 
+     * @param isPlay
+     */
+    private void setPlay(final boolean isPlay) {
+        this.isPlaying = isPlay;
+    }
+
+    /**
+     * 
+     */
+    public void replay() {
+        if (this.isPlaying) {
+            this.player.pause();
+            this.player.rewind();
+        }
+    }
 
 }
