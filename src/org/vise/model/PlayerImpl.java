f319b9ab09a01672a2aab8b1fad37906c18f2dc8
Mattia Navacchia <mattia.navacchia@studio.unibo.it>
2018-04-12 17:39:32 +0200

    Create Player class

--- src/org/vise/model/PlayerImpl.java
+++ src/org/vise/model/PlayerImpl.java
@@ -1,10 +1,105 @@
 package org.vise.model;
 
+import org.vise.model.playlist.FileSystemHandler;
+import org.vise.model.playlist.Song;
+
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+
 /**
  * 
- * @author mattianavach
+ * @author eliapasqualini
  *
  */
 public class PlayerImpl implements Player {
 
+    private AudioPlayer player;
+    private Minim minim;
+    private boolean isPlaying;
+
+    /**
+     * Constructor for the class PlayerImpl.
+     */
+    public PlayerImpl() {
+        this.minim = new Minim(new FileSystemHandler());
+        this.isPlaying = false;
+    }
+
+    /**
+     * 
+     * @param song
+     *          The song to be played.
+     */
+    @Override
+    public void loadSong(final Song song) {
+        this.player = this.minim.loadFile(song.getPath());
+    }
+
+    /**
+     * 
+     */
+    @Override
+    public void play() {
+        this.checkPlayerLoaded();
+        this.player.play();
+        this.setIsPlaying(true);
+    }
+
+    /**
+     * 
+     */
+    @Override
+    public void pause() {
+        this.checkPlayerLoaded();
+        this.player.pause();
+        this.setIsPlaying(false);
+    }
+
+    /**
+     * 
+     */
+    @Override
+    public void replay() {
+        this.checkPlayerLoaded();
+        this.player.pause();
+        this.player.rewind();
+        this.setIsPlaying(false);
+    }
+
+    /**
+     * 
+     * @return
+     *          If the player is playing a song.
+     */
+    @Override
+    public boolean isPlayingSong() {
+        return this.isPlaying;
+    }
+
+    /**
+     * 
+     */
+    private void checkPlayerLoaded() {
+        if (this.playerEmpty()) {
+            throw new IllegalStateException("No song has been loaded.");
+        }
+    }
+
+    /**
+     * 
+     * @return
+     *          Return if there aren't song already loaded.
+     */
+    private boolean playerEmpty() {
+        return this.player == null;
+    }
+
+    /**
+     * 
+     * @param state
+     *          State to be attached to isPlaying field.
+     */
+    private void setIsPlaying(final boolean state) {
+        this.isPlaying = state;
+    }
 }
